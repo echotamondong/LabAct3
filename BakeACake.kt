c18b4c11ffa913e49a7eeb3f3212991d354886f9
echotamondong <113017765+echotamondong@users.noreply.github.com>
2023-09-25 18:24:24 +0800

    Add files via upload

---
+++ BakeACake.kt
@@ -0,0 +1,33 @@
+fun main() {
+    val age =24
+    val layers = 5
+    
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age,layers)
+    
+}
+fun printCakeCandles(age:Int){
+    repeat(age){
+        print("!")
+        
+    }
+    println("")
+    
+}
+fun printCakeTop(age:Int){
+    repeat(age){
+        print("=")
+        
+    }
+    println("")
+}
+fun printCakeBottom(age: Int, layers: Int) {
+    repeat(layers) {
+        repeat(age) {
+            print("@")
+        }
+        println("")
+    }
+}
+
